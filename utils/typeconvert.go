965efd4dd65eb33beb79f87b4dcf7c3c3c768b3a
guanwei <guanwei@lianjia.com>
2017-06-21 12:14:03 +0800

    add type convert in utils

---
+++ utils/typeconvert.go
@@ -0,0 +1,12 @@
+packag utils
+import (
+	"unsafe"
+)
+
+
+func StringToBytes(s string) []byte {
+	    return *(*[]byte)(unsafe.Pointer(&s))
+}
+func BytesToString(b []byte) string {
+	    return *(*string)(unsafe.Pointer(&b))
+}
